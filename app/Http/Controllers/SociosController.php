a71ef1367d3ee78ebb2e58796de7bc76192d8a22
damianperez <perezdamian@gmail.com>
2026-03-28 13:33:29 -0300

    Socios Controller

--- app/Http/Controllers/SociosController.php
+++ app/Http/Controllers/SociosController.php
@@ -17,17 +17,21 @@ class SociosController extends Controller
         $p4 = $request->route('p4');
 
         $SQL = 'SELECT * FROM users';
-        if ($p1) {
-            $SQL .= " WHERE Nro_socio='$p1'";
-        }
+        if ($p1 )
+            if (is_numeric($p1)) 
+                $SQL .= " WHERE Nro_socio='$p1'";
+                else
+                $SQL .= " WHERE (`Apellido y nombre` like '%$p1%' OR `Lugar de pago` like '%$p1%' OR Actividad like '%$p1%')";
+        
+        
         if ($p2) {
-            $SQL .= " AND `Apellido y nombre`='$p2'";
+            $SQL .= " AND ( `Apellido y nombre` like '%$p2%' OR `Lugar de pago` like '%$p2%' OR Actividad like '%$p2%' )    ";
         }
         if ($p3) {
-            $SQL .= " AND `Lugar de pago`='$p3'";
+            $SQL .= " AND ( `Apellido y nombre` like '%$p3%' OR `Lugar de pago` like '%$p3%' OR Actividad like '%$p3%' )    ";
         }
         if ($p4) {
-            $SQL .= " AND Actividad='$p4'";
+            $SQL .= " AND ( `Apellido y nombre` like '%$p4%' OR `Lugar de pago` like '%$p4%' OR Actividad like '%$p4%' )    ";
         }
         $users = DB::connection('mariagasav')->select($SQL);
         return response()->json($users);
